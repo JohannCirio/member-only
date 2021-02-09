c9ddbea356746a22c3f43166d1100a1364ca63fb
Johann <johannocsantos@gmail.com>
2021-02-09 16:40:48 -0300

    Finishes the main part of the project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "posts#index"
+
+  resources :posts, only: [:index]
+  
+  resources :users do
+    resources :posts, only: [:new, :create]
+  end
+
+
 end
